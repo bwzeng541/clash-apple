7055efbfee580a3b84968f9b6337a9b7c4f80fcf
Arror <maqiang@autoai.com>
2022-04-21 17:22:46 +0800

    Simple

--- clash/delay.go
+++ clash/delay.go
@@ -2,73 +2,30 @@ package clash
 
 import (
 	"context"
-	"encoding/json"
 	"time"
 
 	"github.com/Dreamacro/clash/tunnel"
 )
 
-type _URLTestRequest struct {
-	Name    string `json:"name"`
-	URL     string `json:"url"`
-	Timeout int64  `json:"timeout"`
-}
+func URLTest(name string, url string, timeout int64) int64 {
 
-type _URLTestResponse struct {
-	Name  string `json:"name"`
-	Delay int64  `json:"delay"`
-}
+	proxies := tunnel.Proxies()
+	proxy, exist := proxies[name]
 
-func ABCURLTest() ([]byte, error) {
-	return nil, nil
-}
-
-func URLTest(request []byte) []byte {
-	var (
-		req  *_URLTestRequest
-		resp _URLTestResponse
-	)
-	for {
-		req = new(_URLTestRequest)
-		json.Unmarshal(request, req)
-
-		proxies := tunnel.Proxies()
-		proxy, exist := proxies[req.Name]
-
-		if !exist {
-			resp = _URLTestResponse{
-				Name:  req.Name,
-				Delay: -1,
-			}
-			break
-		}
-
-		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*time.Duration(req.Timeout))
-		defer cancel()
-
-		delay, err := proxy.URLTest(ctx, req.URL)
-		if ctx.Err() != nil {
-			resp = _URLTestResponse{
-				Name:  req.Name,
-				Delay: -2,
-			}
-			break
-		}
-
-		if err != nil || delay == 0 {
-			resp = _URLTestResponse{
-				Name:  req.Name,
-				Delay: -3,
-			}
-			break
-		}
-
-		resp = _URLTestResponse{
-			Name:  req.Name,
-			Delay: int64(delay),
-		}
-		break
+	if !exist {
+		return 0
 	}
-	data, _ := json.Marshal(resp)
-	return data
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*time.Duration(timeout))
+	defer cancel()
+
+	delay, err := proxy.URLTest(ctx, url)
+	if ctx.Err() != nil {
+		return -1
+	}
+	if err != nil || delay == 0 {
+		return -2
+	}
+
+	return int64(delay)
 }
