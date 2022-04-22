0018bdce5a05adbae4adf040879be6f343028d07
Arror <maqiang@autoai.com>
2022-04-22 10:39:29 +0800

    Batch url test

--- clash/delay.go
+++ clash/delay.go
@@ -4,28 +4,45 @@ import (
 	"context"
 	"time"
 
+	"github.com/Dreamacro/clash/common/batch"
 	"github.com/Dreamacro/clash/tunnel"
 )
 
-func URLTest(name string, url string, timeout int64) int64 {
+var (
+	_callback URLTestCallback
+)
 
+type URLTestCallback interface {
+	URLTest(name string, delay int64)
+}
+
+func URLTests(callback URLTestCallback, duration int64) {
+	_callback = callback
+	ticker := time.NewTicker(time.Duration(duration) * time.Second)
+	defer ticker.Stop()
+	for range ticker.C {
+		_URLTests()
+	}
+}
+
+func _URLTests() {
+	b, _ := batch.New(context.Background(), batch.WithConcurrencyNum(10))
 	proxies := tunnel.Proxies()
-	proxy, exist := proxies[name]
-
-	if !exist {
-		return 0
+	for _, proxy := range proxies {
+		p := proxy
+		b.Go(p.Name(), func() (any, error) {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+			delay, err := p.URLTest(ctx, "http://www.gstatic.com/generate_204")
+			if ctx.Err() != nil {
+				_callback.URLTest(p.Name(), 0)
+			} else if err != nil || delay == 0 {
+				_callback.URLTest(p.Name(), -1)
+			} else {
+				_callback.URLTest(p.Name(), int64(delay))
+			}
+			return nil, nil
+		})
 	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*time.Duration(timeout))
-	defer cancel()
-
-	delay, err := proxy.URLTest(ctx, url)
-	if ctx.Err() != nil {
-		return -1
-	}
-	if err != nil || delay == 0 {
-		return -2
-	}
-
-	return int64(delay)
+	b.Wait()
 }
